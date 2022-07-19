a5b07860b0bed5afba6d1731e042d6b034717445
arjonas <sidnei.arjonas@gmail.com>
2022-07-19 17:20:03 -0300

    ..
    Correção de erros e endentação

--- main.py
+++ main.py
@@ -1,21 +1,19 @@
 from psutil import virtual_memory
 import tkinter
 
-#Variaveis#
-
 
 #Ambiemte grafico#
 
-
 janela = tkinter.Tk()
 janela.title("Memoria Ram")
-janela.config(height=250,width=250)
+janela.config(height=250, width=250)
 
 
-canvas= tkinter.Canvas(height=100,width=100)
+canvas = tkinter.Canvas(height=100, width=100)
 img = tkinter.PhotoImage(file='pc.png')
-canvas.create_image(100,100,image=img)
-canvas.grid(column=0,row=0)
+canvas.create_image(100, 100, image=img)
+canvas.grid(column=0, row=0)
+
 
 def atualizar():
 
@@ -25,22 +23,24 @@ def atualizar():
     usada_mem = obj_mem[3]
     livre_mem = obj_mem[1]
 
-    free = tkinter.Label(text=f'Memória livre :{round(livre_mem/1000000000,3)} MB', font=('Abys', 10,'bold'))
+    free = tkinter.Label(text=f'Memória livre :{round(livre_mem/1000000000,3)} MB', font=('Abys',
+                                                                                    10 ,'bold'))
     free.grid(column=0, row=1)
 
-    total = tkinter.Label(text=f'Memória total do sistema :{round(total_mem / 1000000000,3)} MB',
-                          font=('Abys', 10, 'bold'))
+    total = tkinter.Label(text=f'Memória total do sistema :{round(total_mem / 1000000000, 3)} MB',
+                                                                        font=('Abys', 10, 'bold'))
     total.grid(column=0, row=3)
 
-    usando = tkinter.Label(text=f'Memória Utilizada pelo sistema :{round(usada_mem / 1000000000,3)} MB ',
+    usando = tkinter.Label(text=f'Memória Utilizada pelo sistema :{round(usada_mem / 1000000000, 3)}'
+                                f'MB ',
                           font=('Abys', 10, 'bold'))
     usando.grid(column=0, row=5)
 
 
-    janela.after(3000,atualizar)
-
+    janela.after(3000, atualizar)
 
 
 atualizar()
 
 janela.mainloop()
+
